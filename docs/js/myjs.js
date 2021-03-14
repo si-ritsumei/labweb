618dc0cdc3dfc691c2ea874538a6994e74000cfe
Yohei Murakami <yohei@fc.ritsumei.ac.jp>
2021-03-14 09:29:28 +0900

    Initialize dropdown after loading everything

--- docs/js/myjs.js
+++ docs/js/myjs.js
@@ -1,2 +1,5 @@
 $('#myNavbarJa').load("../nav-ja.html");
 $('#myNavbar').load("../nav.html");
+window.onload = function() {
+    $('.dropdown-trigger').dropdown({hover: true, constrain_width: false, coverTrigger: false});
+}
