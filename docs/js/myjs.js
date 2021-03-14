c0fda4eea01fc430a012d3955bfd74fea300e232
Yohei Murakami <yohei@fc.ritsumei.ac.jp>
2021-03-14 10:22:46 +0900

    Load footer and beautify every page

--- docs/js/myjs.js
+++ docs/js/myjs.js
@@ -1,5 +1,7 @@
 $('#myNavbarJa').load("../nav-ja.html");
 $('#myNavbar').load("../nav.html");
+$('#myFooterJa').load("../footer-ja.html");
+$('#myFooter').load("../footer.html");
 window.onload = function() {
     $('.dropdown-trigger').dropdown({hover: true, constrain_width: false, coverTrigger: false});
 }
