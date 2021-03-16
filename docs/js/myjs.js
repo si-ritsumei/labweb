062edcaa872352487ce79dcf5b17da7da22bd4c0
Yohei Murakami <yohei@fc.ritsumei.ac.jp>
2021-03-16 17:55:14 +0900

    Manage the url list of navbar in array of javascript.

--- docs/js/myjs.js
+++ docs/js/myjs.js
@@ -1,7 +1,36 @@
-$('#myNavbarJa').load("../nav-ja.html");
-$('#myNavbar').load("../nav.html");
-$('#myFooterJa').load("../footer-ja.html");
+$('#myNavbar').load("../nav.html", addActive);
 $('#myFooter').load("../footer.html");
+
+function addActive(eventObject) {
+      var items = {'ja':['index-ja.html#research', 'lablife-ja.html', 'publications-ja.html', 'joinus-ja.html', 'contact-ja.html'],
+                    'en':['index.html#research', 'lablife.html', 'publications.html', 'joinus.html', 'contact.html']}
+      var contents = $('#nav-pc').children();
+      var activeNum = $('#myNavbar').data("active");
+      var languages = $('#languages, #sidenav-languages').children();
+      var lang = $('#myNavbar').data("language");
+
+      var i = 0;
+      for(i in contents) {
+          // console.log(items[lang][i])
+          // console.log(i)
+          // console.log(contents.eq(i))
+          if(i == 5) break;
+          if(i == activeNum) {
+              contents.eq(i).addClass('active');
+          }
+          contents.eq(i).children().attr('href', items[lang][i]);
+      }
+
+      var i = 0;
+      for(i in languages){
+          if(i/2 == 0){
+              languages.eq(i).children().attr('href', items['ja'][activeNum]);
+          }else{
+              languages.eq(i).children().attr('href', items['en'][activeNum]);
+          }
+      }
+    }
+
 window.onload = function() {
     $('.dropdown-trigger').dropdown({hover: true, constrain_width: false, coverTrigger: false});
 }
